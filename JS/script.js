a078b7d432ff0cd55c2a5286b679fa09d0d3f8d3
Sveatoslav <sveatoslavbodrus@gmail.com>
2022-05-17 15:00:17 +0200

    first functions

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function showFirstMessage(text){
+    console.log(text);
+
+}
+
+showFirstMessage("Hello World");
+
+function calc (a, b){
+    return (a+b);
+}
+console.log(calc(3,4));
+
+const logger = function (){
+    console.log("hello");
+};
+
+logger();
