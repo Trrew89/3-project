fe357507d97cb1a6a0a9b5f14d0188d05f831d5f
Sveatoslav <sveatoslavbodrus@gmail.com>
2022-05-19 11:28:51 +0200

    Homework

--- JS/script.js
+++ JS/script.js
@@ -17,3 +17,26 @@ const logger = function (){
 };
 
 logger();
+
+const calc2 = (a, b) => { return a+b;}; 
+
+
+function getMathResult(int1, int2) {
+    
+    let number = int1;
+    let string = '';
+    if (int2 !== number && int2 <=0){
+        return int1;
+    }else {
+        for (let i =1; i <= int2; i++){
+            if (i == int2){
+            string += `${int1 * i}`;
+        } else{
+            string += `${int1 * i}---`;
+        }
+    return string;
+    }
+}
+}
+
+console.log(getMathResult(5, 3));
